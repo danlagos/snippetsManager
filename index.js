dc924e1fab54f6aa87ff30ad871b8c4f39694c6b
danlagos <lagos.d@gmail.com>
2021-04-20 23:51:15 -0700

    finished server

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 
 // set up server
 
-const app = express();
+ const app = express();
 
 app.listen(5000, ()=> console.log(`server started in port 5000`));
 
+app.get("/test", (req, res) => {
+  res.send(`Hello World`);
+})
